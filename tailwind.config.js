524d48751d79d33310fa9a4561d9056c85c16032
Martin Eberth <mkrebseberth@gmail.com>
2021-02-16 14:21:31 +0100

    Background: color gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: false,
     content: ['./dist/**/*.html'],
   }, 
   darkMode: 'class', // or 'media' or 'class'
